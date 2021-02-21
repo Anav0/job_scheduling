f677a72916a8ca6e5c52bd19f17a0271dcd3b779
Igor <igor_motyka@mail.com>
2021-02-21 16:11:41 +0100

    Now we now if machine can handle given job and how many at the same time

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
 
 pub struct JobSolver {
-    pub machines: Vec<usize>, //how many jobs given machine can handle.
+    pub machines: Vec<Vec<usize>>, //how many jobs of given type given machine can handle.
     pub processing_order: Vec<Vec<usize>>, //order in which given machine need to solve tasks.
 }
 
 impl JobSolver {
-    fn new(machines: Vec<usize>) -> Self {
+    fn new(machines: Vec<Vec<usize>>) -> Self {
         let number_of_machines = machines.len();
         JobSolver {
             machines,
@@ -14,17 +14,23 @@ impl JobSolver {
         }
     }
 
-    fn panic_if_invalid(p: Vec<usize>, d: Vec<usize>, r: Vec<usize>, s: Vec<Vec<usize>>) {
+    fn panic_if_invalid(&self, p: Vec<usize>, d: Vec<usize>, r: Vec<usize>, s: Vec<Vec<usize>>) {
         if p.len() != d.len() || p.len() != r.len() || d.len() != r.len() {
             panic!("'p', 'd', 'r' should be the same length");
         }
-
-        for row in s.iter() {
-            if row.len() != p.len() {
+        for i in 0..s.len() {
+            if s[i].len() != p.len() {
                 panic!(format!("'p'needs to be a {0}x{0} matrix", p.len()));
             }
         }
-
+        for i in 0..self.machines.len() {
+            if self.machines[i].len() != p.len() {
+                panic!(format!(
+                    "All rows of machgines matrix need to be of length: '{0}'",
+                    p.len()
+                ));
+            }
+        }
         for i in 0..p.len() {
             if r[i] > d[i] {
                 panic!("Availability of given task cannot be greater than its due date");
@@ -53,19 +59,19 @@ impl JobSolver {
     }
 
     fn solve(&self, p: Vec<usize>, d: Vec<usize>, r: Vec<usize>, s: Vec<Vec<usize>>) {
-        JobSolver::panic_if_invalid(p, d, r, s);
+        self.panic_if_invalid(p, d, r, s);
     }
 }
 
 fn main() {
     let p = vec![20, 30, 45]; //how much it take to finish given job. (in minutes)
-    let d = vec![60, 60, 60]; //due date
-    let r = vec![10, 5, 0]; //job would be available after x from start date.
+    let d = vec![60 * 4, 60 * 4, 60 * 4]; //due date
+    let r = vec![5, 10, 15]; //job would be available after x from start date.
 
     let mut s = vec![vec![0; p.len()]; p.len()]; //time delay neccessary between jobs.
     s[0][1] = 5;
 
-    let solver = JobSolver::new(vec![1, 1]);
+    let solver = JobSolver::new(vec![vec![1, 1, 0], vec![2, 2, 1]]); //First machne can process one of job 1 and 2 but cannot process job 3
 
     solver.solve(p, d, r, s);
 }
